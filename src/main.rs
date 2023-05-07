6f2af096e29e4c1ee940043e1ecefdb3449fe783
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-05-07 21:24:20 +0200

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 
-use actix_web::{ HttpServer, HttpResponse, Responder, App, get, web };
+use actix_web::{ HttpServer, HttpResponse, Responder, App, get, web, middleware::Logger };
 use actix_files as fs;
+use env_logger::Env;
+use log::{ info, debug };
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct AppConfig {
     port: u16,
     ip: String,
@@ -17,6 +19,10 @@ async fn health() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Logger
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // App Config
     let app_config = AppConfig {
         ip: env::var("HOSTNAME").unwrap_or("0.0.0.0".to_string()),
         port: env::var("PORT").unwrap_or("8080".to_string()).parse().unwrap_or(8080),
@@ -25,9 +31,15 @@ async fn main() -> std::io::Result<()> {
 
     let app_config_clone = app_config.clone();
 
+    info!("Server started!");
+    debug!("App Config: {:#?}", app_config);
+
     HttpServer::new(move || {
+        let logger = Logger::default();
+
         App::new()
             .app_data(web::Data::new(app_config.clone()))
+            .wrap(logger)
             .service(health)
             .service(fs::Files::new("/fdroid", &app_config.repo_path))
     })
