9bf323db55851f77b7371aa02d595faef39a747b
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-05-15 20:46:10 +0200

    todo's

--- src/routes/app.rs
+++ src/routes/app.rs
@@ -1,1 +1,3 @@
 //! Route used to edit apps and their metadata
+
+// TODO: add apps, get apps, update app metadata
--- src/routes/config.rs
+++ src/routes/config.rs
@@ -3,6 +3,8 @@ use actix_web::{ get, web::{ self, Json } };
 
 use crate::utils::{ config_editor::{ self, ConfigFile }, app_config::AppConfig, error::Result };
 
+// TODO: update general info, backup keystore (get keystore and password), update store picture
+
 #[get("")]
 async fn get_config(config: web::Data<AppConfig>) -> Result<Json<ConfigFile>> {
     let config = config_editor::get_config(&(config.repo_path.clone() + "/config.yml"));
