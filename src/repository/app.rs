b7c8711551a15d88ee4ffcc54ccf27d4946056e3
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-06-18 21:41:06 +0200

    apps p1

--- src/repository/app.rs
+++ src/repository/app.rs
@@ -1,2 +1,97 @@
-#[derive(Clone)]
-pub struct App {}
+use serde::Serialize;
+
+use crate::utils::error::{Error, Result};
+
+use super::Repository;
+
+#[derive(Clone, Serialize)]
+pub struct App {
+  categories: Vec<String>,
+  suggestedVersionCode: String,
+  license: String,
+  name: String,
+  added: i64,
+  packageName: String,
+  lastUpdated: i64,
+  packages: Vec<Package>,
+}
+
+impl App {
+  fn from_json(value: serde_json::Value) -> Option<Vec<Self>> {
+    let apps = value.get("apps")?;
+    let packages = value.get("packages")?;
+
+    let mut apps_vec = vec![];
+
+    for app in apps.as_array()? {
+      let name = app.get("name")?.as_str()?.to_owned();
+      let suggestedVersionCode = app.get("suggestedVersionCode")?.as_str()?.to_owned();
+      let license = app.get("license")?.as_str()?.to_owned();
+      let packageName = app.get("packageName")?.as_str()?.to_owned();
+      let lastUpdated = app.get("lastUpdated")?.as_i64()?.to_owned();
+      let added = app.get("added")?.as_i64()?.to_owned();
+
+      let mut categories = vec![];
+
+      let mut packages = vec![];
+
+      // TODO
+      todo!();
+
+      apps_vec.push(App {
+        name,
+        suggestedVersionCode,
+        license,
+        packageName,
+        lastUpdated,
+        added,
+        packages,
+        categories,
+      });
+    }
+
+    Some(apps_vec)
+  }
+}
+
+#[derive(Clone, Serialize)]
+pub struct Package {
+  added: i64,
+  apkName: String,
+  hash: String,
+  hashType: String,
+  maxSdkVersion: u32,
+  minSdkVersion: u32,
+  nativecode: Vec<String>,
+  packageName: String,
+  sig: String,
+  signer: String,
+  size: u64,
+  targetSdkVersion: u32,
+  usesPermission: Vec<(String, u32)>,
+  usesPermissionSdk23: Vec<(String, u32)>,
+  versionCode: u64,
+  versionName: String,
+}
+
+impl TryFrom<serde_json::Value> for Package {
+  type Error = Error;
+
+  fn try_from(value: serde_json::Value) -> Result<Self> {
+    todo!()
+  }
+}
+
+impl Repository {
+  pub fn get_apps(&self) -> Result<Vec<App>> {
+    App::from_json(
+      serde_json::from_str(self.path.join("/repo/index-v1.json").to_str().ok_or(
+        Error::JsonConvertError("Could not read repository index file!".to_owned()),
+      )?)
+      .map_err(|_| Error::JsonConvertError("Could not read repository index file!".to_owned()))?,
+    )
+    .ok_or(Error::JsonConvertError(
+      "Could not read repository index file!".to_owned(),
+    ))
+  }
+}
--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,52 +1,55 @@
 use core::fmt;
-use std::{ io, error };
+use std::{error, io};
 
-use actix_web::{ ResponseError, http::StatusCode };
+use actix_web::{http::StatusCode, ResponseError};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    FileError(io::Error),
-    YAMLConvertError(serde_yaml::Error),
+  FileError(io::Error),
+  YAMLConvertError(serde_yaml::Error),
+  JsonConvertError(String),
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::FileError(err) => write!(f, "Error while working with a file: {:#?}", err),
-            Error::YAMLConvertError(err) =>
-                write!(f, "Error while converting a yml file: {:#?}", err),
-        }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Error::FileError(err) => write!(f, "Error while working with a file: {err:#?}"),
+      Error::YAMLConvertError(err) => write!(f, "Error while converting a yml file: {err:#?}"),
+      Error::JsonConvertError(err) => write!(f, "Error while converting a json file: {err}"),
     }
+  }
 }
 
 impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            Error::FileError(err) => Some(err),
-            Error::YAMLConvertError(err) => Some(err),
-        }
+  fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+    match self {
+      Error::FileError(err) => Some(err),
+      Error::YAMLConvertError(err) => Some(err),
+      Error::JsonConvertError(err) => None,
     }
+  }
 }
 
 impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Self::FileError(error)
-    }
+  fn from(error: io::Error) -> Self {
+    Self::FileError(error)
+  }
 }
 
 impl From<serde_yaml::Error> for Error {
-    fn from(error: serde_yaml::Error) -> Self {
-        Self::YAMLConvertError(error)
-    }
+  fn from(error: serde_yaml::Error) -> Self {
+    Self::YAMLConvertError(error)
+  }
 }
 
 impl ResponseError for Error {
-    fn status_code(&self) -> actix_web::http::StatusCode {
-        match self {
-            Error::FileError(err) => err.status_code(),
-            Error::YAMLConvertError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        }
+  fn status_code(&self) -> actix_web::http::StatusCode {
+    match self {
+      Error::FileError(err) => err.status_code(),
+      Error::YAMLConvertError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+      Error::JsonConvertError(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
+  }
 }
