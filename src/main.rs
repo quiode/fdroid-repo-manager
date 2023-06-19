f8ecad967ce71f6140c6a39095e45cabd7a6cd11
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-06-19 21:44:15 +0200

    get apps route but throws error

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,83 @@
-mod utils;
-mod routes;
 mod guards;
 mod repository;
+mod routes;
+mod utils;
 
-use actix_web::middleware;
-use actix_web::{ HttpServer, HttpResponse, Responder, App, get, web, middleware::Logger };
 use actix_files as fs;
+use actix_web::middleware;
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
-use log::{ info, debug };
+use log::{debug, info};
 use repository::Repository;
 
-use crate::routes::config::{ get_config, post_config };
-use crate::utils::app_config::{AppConfig, WrappedValue};
 use crate::guards::auth_guard::AuthGuard;
+use crate::routes::app::get_apps;
+use crate::routes::config::{get_config, post_config};
+use crate::utils::app_config::{AppConfig, WrappedValue};
 
 #[get("/health")]
 async fn health() -> impl Responder {
-    HttpResponse::Ok().body("Ok!")
+  HttpResponse::Ok().body("Ok!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+  // Logger
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    // App Config
-    let app_config = AppConfig::from_env();
+  // App Config
+  let app_config = AppConfig::from_env();
 
-    let app_config_clone = app_config.clone();
+  let app_config_clone = app_config.clone();
 
-    info!("Server started!");
-    debug!("App Config: {:#?}", app_config);
+  info!("Server started!");
+  debug!("App Config: {:#?}", app_config);
 
-    let fdroid_repository = web::Data::new(Repository::new(app_config.repo_path.value().clone()));
-    HttpServer::new(move || {
-        let logger = Logger::default();
+  let fdroid_repository = web::Data::new(Repository::new(app_config.repo_path.value().clone()));
+  HttpServer::new(move || {
+    let logger = Logger::default();
 
-        App::new()
-            // provide app config
-            .app_data(web::Data::new(app_config.clone()))
-            // provide fdroid repository
-            .app_data(fdroid_repository.clone())
-            // normalize routes (add / to all routes)
-            .wrap(middleware::NormalizePath::new(middleware::TrailingSlash::Trim))
-            // add logger as middleware
-            .wrap(logger)
-            // health service for HEALTHCHECK in docker
-            .service(health)
-            // fdroid repo for fdroid
-            .service(
-                fs::Files
-                    ::new("/fdroid", app_config.repo_path.value())
-                    .show_files_listing()
-                    // remove acces to hidden files
-                    .path_filter(|path, _| {
-                        // files that shouldn't be accessible
-                        let hidden_files = vec!["config.yml", "keystore.p12"];
-                        path.file_name()
-                            .map(|file_name| !hidden_files.contains(&file_name.to_str().unwrap()))
-                            .unwrap_or(true)
-                    })
-            )
-            // config services for manipulating fdroid config file
-            .service(
-                web::scope("/config").service(get_config).service(post_config).guard(AuthGuard)
-            )
-    })
-        .bind((app_config_clone.ip.value().clone(), app_config_clone.port.value().clone()))?
-        .run().await
+    App::new()
+      // provide app config
+      .app_data(web::Data::new(app_config.clone()))
+      // provide fdroid repository
+      .app_data(fdroid_repository.clone())
+      // normalize routes (add / to all routes)
+      .wrap(middleware::NormalizePath::new(
+        middleware::TrailingSlash::Trim,
+      ))
+      // add logger as middleware
+      .wrap(logger)
+      // health service for HEALTHCHECK in docker
+      .service(health)
+      // fdroid repo for fdroid
+      .service(
+        fs::Files::new("/fdroid", app_config.repo_path.value())
+          .show_files_listing()
+          // remove acces to hidden files
+          .path_filter(|path, _| {
+            // files that shouldn't be accessible
+            let hidden_files = vec!["config.yml", "keystore.p12"];
+            path
+              .file_name()
+              .map(|file_name| !hidden_files.contains(&file_name.to_str().unwrap()))
+              .unwrap_or(true)
+          }),
+      )
+      // config services for manipulating fdroid config file
+      .service(
+        web::scope("/config")
+          .service(get_config)
+          .service(post_config)
+          .guard(AuthGuard),
+      )
+      // app services for manipulating apps
+      .service(web::scope("/apps").service(get_apps).guard(AuthGuard))
+  })
+  .bind((
+    app_config_clone.ip.value().clone(),
+    app_config_clone.port.value().clone(),
+  ))?
+  .run()
+  .await
 }
--- src/routes/app.rs
+++ src/routes/app.rs
@@ -1,7 +1,19 @@
 //! Route used to edit apps and their metadata
 
+use actix_web::{get, web, Result, Responder};
+
+use crate::repository::Repository;
+
 // TODO: add apps, get apps, update app metadata
 // TODO: cleanup: "fdroid rewritemeta"
 // TODO: "clean" (delete all metadatas and apk's)
 // TODO: categories management
 // TODO: sign apks
+#[get("")]
+async fn get_apps(repo: web::Data<Repository>) -> Result<impl Responder> {
+    let apps = repo.get_apps()?;
+
+    todo!("Fix this route");
+
+    Ok(web::Json(apps))
+}
