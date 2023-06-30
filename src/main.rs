3b80fba5cb1aaab62363eb34616f87e143f2a075
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-06-30 23:18:27 +0200

    add todo

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ async fn health() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  // TODO: Tests
   // Logger
   env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
