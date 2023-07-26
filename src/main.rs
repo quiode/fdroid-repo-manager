3caf4ac72347409e996ae0c0702e5f8ca7630767
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-07-26 18:55:46 +0200

    remove todo

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ mod guards;
 mod routes;
 mod utils;
 
-// TODO: refactor project structure and crate visibilities, maybe consider splitting into a library?
 // TODO: update README.md
 
 #[get("/health")]
