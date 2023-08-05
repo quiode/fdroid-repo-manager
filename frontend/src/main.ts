33a9d75b42adc80424747adce622aaa466c1a54c
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-08-05 21:19:52 +0200

    add vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,25 @@
 import './assets/styles.scss';
 
+// Vue
 import { createApp } from 'vue';
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+
+// Pinia
 import { createPinia } from 'pinia';
 
+// Custom
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+const vuetify = createVuetify();
+
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
