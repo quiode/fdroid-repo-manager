7dc790fb855710b037b2e208c5b86a91fc4920d4
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-08-05 18:09:48 +0200

    NotFound page, remove unused files

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,22 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: () => import('../views/HomeView.vue')
     },
+    // will match everything and put it under `$route.params.pathMatch`
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue')
+    },
   ]
 })
 
