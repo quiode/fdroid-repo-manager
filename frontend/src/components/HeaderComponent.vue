a14a30b7da9d2974e6da66207ef631c734770379
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-02-07 17:17:15 +0100

    apps clickable

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -5,16 +5,20 @@ function onSettings() {
 </script>
 
 <template>
-<nav class="row m-0">
-  <div class="col-5"></div>
-  <h1 class="col-2 text-center h-100">Apps</h1>
-  <div class="col-4"></div>
-  <div class="col-1">
-    <div class="d-flex justify-content-center align-content-end w-100 h-100 pb-2">
-      <h1 @click="onSettings" style="height: min-content;" class="my-auto pointer"><i class="bi bi-gear-fill"></i></h1>
+  <nav class="row m-0">
+    <div class="col-5"></div>
+    <h1 class="col-2 text-center h-100">
+      <router-link to="/">
+        Apps
+      </router-link>
+    </h1>
+    <div class="col-4"></div>
+    <div class="col-1">
+      <div class="d-flex justify-content-center align-content-end w-100 h-100 pb-2">
+        <h1 @click="onSettings" style="height: min-content;" class="my-auto pointer"><i class="bi bi-gear-fill"></i></h1>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <style scoped>
@@ -26,6 +30,10 @@ nav {
 }
 
 h1 {
- font-size: 3vh !important;
+  font-size: 3vh !important;
+}
+
+a {
+  color: inherit;
 }
 </style>
